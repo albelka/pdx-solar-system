924e9c6c13d74823ebbd078d26bba093d8463865
Anne Belka and Michael Andrade <michael@>
2016-11-07 14:53:55 -0800

    fill out properties of celestial bodies

--- js/scripts.js
+++ js/scripts.js
@@ -28,19 +28,25 @@ var Venus = new CelestialBody();
 Venus.bodyName = "Venus";
 Venus.orbits = Sol;
 Venus.satellites = [];
-Venus.circumference = 9525.1;
-Venus.distance = 36000000;
+Venus.circumference = 23627;
+Venus.distance = 21402600;
 Venus.pdxLocation = "img/mercuryMap.jpg";
-Venus.details = ["represented by a hematite", "mostly ferrous metals"];
 
 var Earth = new CelestialBody();
 Earth.bodyName = "Earth";
 Earth.orbits = Sol;
-Earth.satellites = [];
+Earth.satellites = [Luna];
 Earth.circumference = 9525.1;
 Earth.distance = 36000000;
-Earth.pdxLocation = "img/mercuryMap.jpg";
-Earth.details = ["represented by a hematite", "mostly ferrous metals"];
+Earth.pdxLocation = "img/earthMap.jpg";
+
+var Luna = new CelestialBody();
+Luna.bodyName = "Luna";
+Luna.orbits = Earth;
+Luna.satellites = [];
+Luna.circumference = 6785;
+Luna.distance = 238855;
+Luna.pdxLocation = "img/earthMap.jpg";
 
 var Mars = new CelestialBody();
 Mars.bodyName = "Mars";
@@ -49,61 +55,135 @@ Mars.satellites = [];
 Mars.circumference = 9525.1;
 Mars.distance = 36000000;
 Mars.pdxLocation = "img/mercuryMap.jpg";
-Mars.details = ["represented by a hematite", "mostly ferrous metals"];
+
+var Deimos = new CelestialBody();
+Deimos.bodyName = "Deimos";
+Deimos.orbits = Mars;
+Deimos.satellites = [];
+Deimos.circumference = 15;
+Deimos.distance = 45794;
+Deimos.pdxLocation = "img/mercuryMap.jpg";
+
+var Phobos = new CelestialBody();
+Phobos.bodyName = "Phobos";
+Phobos.orbits = Mars;
+Phobos.satellites = [];
+Phobos.circumference = 46.8;
+Phobos.distance = 18095;
+Phobos.pdxLocation = "img/mercuryMap.jpg";
 
 var Jupiter = new CelestialBody();
 Jupiter.bodyName = "Jupiter";
 Jupiter.orbits = Sol;
-Jupiter.satellites = [];
-Jupiter.circumference = 9525.1;
-Jupiter.distance = 36000000;
+Jupiter.satellites = ["Europa", "Ganymede", "Callisto", "Io"];
+Jupiter.circumference = 272946;
+Jupiter.distance = 483800000;
 Jupiter.pdxLocation = "img/mercuryMap.jpg";
-Jupiter.details = ["represented by a hematite", "mostly ferrous metals"];
+
+var Europa = new CelestialBody();
+Europa.bodyName = "Europa";
+Europa.orbits = Jupiter;
+Europa.satellites = [];
+Europa.circumference = 46.8;
+Europa.distance = 414000;
+Europa.pdxLocation = "img/mercuryMap.jpg";
+
+var Ganymede = new CelestialBody();
+Ganymede.bodyName = "Ganymede";
+Ganymede.orbits = Jupiter;
+Ganymede.satellites = [];
+Ganymede.circumference = 10273;
+Ganymede.distance = 665000;
+Ganymede.pdxLocation = "img/mercuryMap.jpg";
+
+var Callisto = new CelestialBody();
+Callisto.bodyName = "Callisto";
+Callisto.orbits = Jupiter;
+Callisto.satellites = [];
+Callisto.circumference = 9410.3;
+Callisto.distance = 1169855;
+Callisto.pdxLocation = "img/mercuryMap.jpg";
+
+var Io = new CelestialBody();
+Io.bodyName = "Io";
+Io.orbits = Jupiter;
+Io.satellites = [];
+Io.circumference = 7111.9;
+Io.distance = 262,219 ;
+Io.pdxLocation = "img/mercuryMap.jpg";
 
 var Saturn = new CelestialBody();
 Saturn.bodyName = "Saturn";
 Saturn.orbits = Sol;
 Saturn.satellites = [];
-Saturn.circumference = 9525.1;
-Saturn.distance = 36000000;
+Saturn.circumference = 235298;
+Saturn.distance = 888200000;
 Saturn.pdxLocation = "img/mercuryMap.jpg";
-Saturn.details = ["represented by a hematite", "mostly ferrous metals"];
 
 var Ismene = new CelestialBody();
 Ismene.bodyName = "Ismene";
 Ismene.orbits = Sol;
 Ismene.satellites = [];
-Ismene.circumference = 9525.1;
-Ismene.distance = 36000000;
+Ismene.circumference = 166;
+Ismene.distance = 370337230;
 Ismene.pdxLocation = "img/mercuryMap.jpg";
-Ismene.details = ["represented by a hematite", "mostly ferrous metals"];
 
 var Uranus = new CelestialBody();
 Uranus.bodyName = "Uranus";
 Uranus.orbits = Sol;
-Uranus.satellites = [];
+Uranus.satellites = [Oberon, Titania];
 Uranus.circumference = 9525.1;
 Uranus.distance = 36000000;
 Uranus.pdxLocation = "img/mercuryMap.jpg";
-Uranus.details = ["represented by a hematite", "mostly ferrous metals"];
+
+var Oberon = new CelestialBody();
+Oberon.bodyName = "Oberon";
+Oberon.orbits = Uranus;
+Oberon.satellites = [];
+Oberon.circumference = 2971;
+Oberon.distance = 362880;
+Oberon.pdxLocation = "img/mercuryMap.jpg";
+
+var Titania = new CelestialBody();
+Titania.bodyName = "Titania";
+Titania.orbits = Uranus;
+Titania.satellites = [];
+Titania.circumference = 3080;
+Titania.distance = 271000;
+Titania.pdxLocation = "img/mercuryMap.jpg";
+
 
 var Neptune = new CelestialBody();
 Neptune.bodyName = "Neptune";
 Neptune.orbits = Sol;
-Neptune.satellites = [];
-Neptune.circumference = 9525.1;
-Neptune.distance = 36000000;
+Neptune.satellites = [Triton];
+Neptune.circumference = 96685;
+Neptune.distance = 2795000000;
 Neptune.pdxLocation = "img/mercuryMap.jpg";
-Neptune.details = ["represented by a hematite", "mostly ferrous metals"];
+
+var Triton = new CelestialBody();
+Triton.bodyName = "Triton";
+Triton.orbits = Neptune;
+Triton.satellites = [];
+Triton.circumference = 5284;
+Triton.distance = 205000;
+Triton.pdxLocation = "img/mercuryMap.jpg";
 
 var Pluto = new CelestialBody();
 Pluto.bodyName = "Pluto";
 Pluto.orbits = Sol;
-Pluto.satellites = [];
-Pluto.circumference = 9525.1;
-Pluto.distance = 36000000;
+Pluto.satellites = [Charon];
+Pluto.circumference = 4494;
+Pluto.distance = 3670000000;
 Pluto.pdxLocation = "img/mercuryMap.jpg";
-Pluto.details = ["represented by a hematite", "mostly ferrous metals"];
+
+var Charon = new CelestialBody();
+Charon.bodyName = "Charon";
+Charon.orbits = Pluto;
+Charon.satellites = [];
+Charon.circumference = 1190;
+Charon.distance = 12200;
+Charon.pdxLocation = "img/mercuryMap.jpg";
 
 
 
@@ -119,7 +199,7 @@ Pluto.details = ["represented by a hematite", "mostly ferrous metals"];
 
 //Methods for Celestial bodies
 CelestialBody.prototype.quickStats = function () {
-  return [this.bodyName, this.circumference, this.distance];
+  return this.bodyName;
 };
 
 //fullstats function that returns every property?
